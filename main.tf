24f7f95d639c23f172b341de2d759bfce50ef3a4
nag.nampalli@gmail.com <NN061109@Cerner.net>
2026-04-29 09:46:18 +0400

    corrected main file

--- main.tf
+++ main.tf
@@ -14,6 +14,18 @@ data "oci_identity_availability_domains" "ads" {
 }
 
 # -----------------------------
+# Image Lookup (IMPORTANT)
+# -----------------------------
+data "oci_core_images" "oracle_linux" {
+  compartment_id           = var.compartment_ocid
+  operating_system         = "Oracle Linux"
+  operating_system_version = "8"
+  shape                    = "VM.Standard.E2.1.Micro"
+  sort_by                  = "TIMECREATED"
+  sort_order               = "DESC"
+}
+
+# -----------------------------
 # VCN
 # -----------------------------
 resource "oci_core_vcn" "web_vcn" {
